7d9434074d766092f2cb35611347ce3adf8922ec
sivavithu <sivavithu15@live.com>
2024-12-28 19:47:13 +0530

    Update getTransaction.ts

--- app/actions/getTransaction.ts
+++ app/actions/getTransaction.ts
@@ -1,6 +1,6 @@
 'use server';
 import { db } from "@/lib/db";
-import { auth } from '@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { Transaction } from '../../types/Transaction';
 
 async function getTransactions(): Promise<{

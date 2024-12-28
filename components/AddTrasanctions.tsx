afcdc755030338b4ba3c75a6538df9119401a426
sivavithu <sivavithu15@live.com>
2024-12-28 20:04:38 +0530

    Update AddTrasanctions.tsx

--- components/AddTrasanctions.tsx
+++ components/AddTrasanctions.tsx
@@ -1,43 +1,45 @@
 'use client';
 import addTransaction from '../app/actions/addTransaction';
-import {toast} from 'react-toastify'
-import {useRef} from 'react';
-
-
+import { toast } from 'react-toastify';
+import { useRef } from 'react';
 
 const AddTransactions = () => {
-    const formRef=useRef<HTMLFormElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
 
-    const clientAction=async (formData:FormData) => {
-    
-      const  {data,error}=await addTransaction(formData);
-      if(error){
-        toast.error(error)
-      }
-      else{
-        toast.success('transaction added')
-        formRef.Current?.reset()
-
-      }
-        
+  const clientAction = async (formData: FormData) => {
+    const { data, error } = await addTransaction(formData);
+    if (error) {
+      toast.error(error);
+    } else {
+      toast.success('Transaction added');
+      formRef.current?.reset(); // Corrected from `Current` to `current`
     }
+  };
 
-
-
-    return ( <>
-    <h3>Add Transactions</h3>
-    <form ref={formRef} action={clientAction}>
-        <div className='form-control'>
-            <label htmlFor="text">Text</label>
-            <input type="text" id="text" name="text" placeholder="entertext .."/>
-
+  return (
+    <>
+      <h3>Add Transactions</h3>
+      <form ref={formRef} action={clientAction}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" id="text" name="text" placeholder="Enter text..." />
         </div>
-        <div className='form-control'>
-            <label htmlFor="amount">Amount<br/>(negative-expense,positive-income)</label>
-            <input type="amount" id="amount" name="amount" placeholder="enterzNumber .." step="0.01"/>
- <button className='btn'>Add transaction</button>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount<br />(negative-expense, positive-income)
+          </label>
+          <input
+            type="number" // Changed input type to "number" for better validation
+            id="amount"
+            name="amount"
+            placeholder="Enter number..."
+            step="0.01"
+          />
+          <button className="btn">Add Transaction</button>
         </div>
-        </form></> );
-}
- 
+      </form>
+    </>
+  );
+};
+
 export default AddTransactions;
